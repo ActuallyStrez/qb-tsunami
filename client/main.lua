ea3d809853edffba9fb8ab48e46941c6feeb8eec
Joshua <56806832+ActuallyStrez@users.noreply.github.com>
2021-12-23 14:48:40 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -8,18 +8,17 @@ function TsunamiEffect()
         RestorePlayerStamina(PlayerId(), 1.0)
         if math.random(1, 100) < 51 then
             ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.09)
+            if IsPedRunning(PlayerPedId()) and math.random(1,10) < 5 then
+                SetPedToRagdoll(PlayerPedId(), math.random(1000, 3000), math.random(1000, 3000), 3, 0, 0, 0)
+            end
         end
     end
-    if IsPedRunning(PlayerPedId()) then
-        SetPedToRagdoll(PlayerPedId(), math.random(1000, 3000), math.random(1000, 3000), 3, 0, 0, 0)
-    end
-
     startStamina = 0
 end
 
 RegisterNetEvent('google:client:SendMailAlerts', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
-    Wait(math.random(5000, 10000))
+    Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
         sender = "District of Los Santos",
         subject = "Emergency Alert",
@@ -30,17 +29,15 @@ end)
 
 RegisterNetEvent('strez:client:SendMailTsunami', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
-    Wait(math.random(5000, 10000))
+    Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
         sender = "District of Los Santos",
         subject = "Emergency Alert",
         message = "The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 15 minutes! Please start heading home or somewhere local for SAFETY!",
         button = {}
     })
-    Wait(math.random(5000, 10000))
+    Wait(math.random(15000, 25000))
     TriggerEvent('qb-weathersync:client:TsunamiAlert')
-    Wait(math.random(5000, 10000))
+    Wait(math.random(13000, 23000))
     TsunamiEffect()
-    Wait(30000)
-    TriggerServerEvent('qb-multicharacter:server:disconnect')
 end)
