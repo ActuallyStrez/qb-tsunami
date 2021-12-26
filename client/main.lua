c9b353096f4a3e66b8fca7d9c09f053b6c276ac6
Joshua <56806832+ActuallyStrez@users.noreply.github.com>
2021-12-26 14:08:28 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -8,10 +8,10 @@ function TsunamiEffect()
         Wait(1000)
         startStamina = startStamina - 1
         RestorePlayerStamina(PlayerId(), 1.0)
-        if math.random(1, 100) < 11 then
+        if math.random(1, 100) < 9 then
             if not IsPedRagdoll(PlayerPedId()) and IsPedOnFoot(PlayerPedId()) and not IsPedSwimming(PlayerPedId()) then
-                ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.09) -- change this float to increase/decrease camera shake
-                SetPedToRagdollWithFall(PlayerPedId(), 7500, 9000, 1, GetEntityForwardVector(PlayerPedId()), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+                ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.08) -- change this float to increase/decrease camera shake
+                SetPedToRagdollWithFall(PlayerPedId(), 0, 0, 1, GetEntityForwardVector(PlayerPedId()), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
             end
         end
     end
@@ -24,8 +24,8 @@ RegisterNetEvent('strez:client:SendMailAlertsBasic', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
     Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
-        sender = "District of Los Santos",
-        subject = "Emergency Alert",
+        sender = 'District of Los Santos',
+        subject = 'Emergency Alert',
         message = text,
         button = {}
     })
@@ -37,13 +37,13 @@ RegisterNetEvent('strez:client:SendMailTsunamiManual', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
     Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
-        sender = "District of Los Santos",
-        subject = "Emergency Alert",
+        sender = 'District of Los Santos',
+        subject = 'Emergency Alert',
         message = text,
         button = {}
     })
     Wait(math.random(30000, 60000))
-    TriggerServerEvent('qb-weathersync:server:setWeather', "THUNDER")
+    TriggerServerEvent('qb-weathersync:server:setWeather', 'THUNDER')
     Wait(math.random(25000, 35000))
     TsunamiEffect()
 end)
@@ -54,13 +54,13 @@ RegisterNetEvent('strez:client:SendMailTsunamiManualOverride', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
     Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
-        sender = "District of Los Santos",
-        subject = "Emergency Alert",
-        message = "The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 15 minutes! Please start heading home or somewhere local for SAFETY!",
+        sender = 'District of Los Santos',
+        subject = 'Emergency Alert',
+        message = 'The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 15 minutes! Please start heading home or somewhere local for SAFETY!',
         button = {}
     })
     Wait(math.random(30000, 60000))
-    TriggerServerEvent('qb-weathersync:server:setWeather', "THUNDER")
+    TriggerServerEvent('qb-weathersync:server:setWeather', 'THUNDER')
     Wait(math.random(25000, 35000))
     TsunamiEffect()
 end)
@@ -71,13 +71,13 @@ RegisterNetEvent('strez:client:SendMailTsunamiAuto15', function(text)
         TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
         Wait(math.random(7000, 13000))
         TriggerServerEvent('qb-phone:server:sendNewMail', {
-            sender = "District of Los Santos",
-            subject = "Emergency Alert",
-            message = "The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 15 minutes! Please start heading home or somewhere local for SAFETY!",
+            sender = 'District of Los Santos',
+            subject = 'Emergency Alert',
+            message = 'The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 15 minutes! Please start heading home or somewhere local for SAFETY!',
             button = {}
         })
         Wait(math.random(15000, 25000))
-        TriggerServerEvent('qb-weathersync:server:setWeather', "THUNDER")
+        TriggerServerEvent('qb-weathersync:server:setWeather', 'THUNDER')
         Wait(math.random(13000, 23000))
         TsunamiEffect()
 end)
@@ -90,9 +90,9 @@ RegisterNetEvent('strez:client:SendMailTsunamiAuto5', function(text)
     Wait(35000)
 	TriggerEvent('InteractSound_CL:PlayOnOne', 'raidsiren', 0.2)
     TriggerServerEvent('qb-phone:server:sendNewMail', {
-        sender = "District of Los Santos",
-        subject = "Emergency Alert",
-        message = "The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 5 minutes! Los Santos is losing City Power, Please start heading home or somewhere local for SAFETY!",
+        sender = 'District of Los Santos',
+        subject = 'Emergency Alert',
+        message = 'The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 5 minutes! Los Santos is losing City Power, Please start heading home or somewhere local for SAFETY!',
         button = {}
     })
     Wait(math.random(10000, 20000))
@@ -107,9 +107,9 @@ RegisterNetEvent('strez:client:SendMailTsunamiAuto1', function(text)
     TriggerEvent('InteractSound_CL:PlayOnOne', 'Alert', 0.7)
     Wait(math.random(7000, 13000))
     TriggerServerEvent('qb-phone:server:sendNewMail', {
-        sender = "District of Los Santos",
-        subject = "Emergency Alert",
-        message = "The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 1 minutes! Please start heading home or somewhere local for SAFETY!",
+        sender = 'District of Los Santos',
+        subject = 'Emergency Alert',
+        message = 'The National Weather Service has issued a TSUNAMI WARNING for Los Santos, In 1 minutes! Please start heading home or somewhere local for SAFETY!',
         button = {}
     })
     Wait(math.random(13000, 23000))
