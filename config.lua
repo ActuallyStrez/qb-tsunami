623b65a25bc21f40ed3d1b2041338cc493e216cd
DJ On Top <107875648+djontop@users.noreply.github.com>
2024-01-29 18:36:30 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,3 +2,5 @@
 Config = Config or {}
 
 Config.Notify = 'email' -- 'qb' for QBCore Notify / 'ox' for ox_lib / 'email' for QBCore Default Phone
+
+Config.PhoneType = 'qb' --- if Email Enable - 'qb' for qb-phone / 'qs' for quasar phone
